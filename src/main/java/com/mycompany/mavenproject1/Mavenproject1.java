d8a0f64d007a6f9199682f0fea7e0503df410c48
ep9545350-glitch <ep9545350@gmail.com>
2025-10-22 14:06:50 -0500

    Add files via upload

---
+++ src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author LAB-USR-TRUJILLO
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+       int b=20;
+       int a=10;
+       
+        System.out.println(a+b);
+    }
+}
